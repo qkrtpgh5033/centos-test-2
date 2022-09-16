22d2d5dbaf53882df9596686bc49ffa723ee6d91
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-09-16 09:37:41 +0900

    chore : yml 파일 추가

---
+++ src/main/java/com/example/centostest/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.centostest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String showMain(){
+        return "<h1>Hello, sb</h1>";
+    }
+}
