9743f8d863c8cc43bf44b78650ca234e002c6299
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-09-16 09:29:30 +0900

    Initial commit

---
+++ src/main/java/com/example/centostest/CentosTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.centostest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentosTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentosTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/centostest/CentosTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centostest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentosTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
